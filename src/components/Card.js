9ba23ff51c4058fd4b11b9243157440d8d5ee95b
pankaj sharma <psbhardwaj.psb@gmail.com>
2022-04-13 16:16:08 +0530

    initial push

---
+++ src/components/Card.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Card = () => {
+  return <div className="bg-blue-600 h-[30px] w-[30px] rounded-md">hello</div>;
+};
+
+export default Card;
---
+++ src/components/Dashboard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <div
+        id="header"
+        className="min-w-screen text-center flex flex-col items-center justify-center text-white text-3xl p-auto bg-gray-600 h-[100px]"
+      >
+        Hello Pankaj, Welcome !!
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
